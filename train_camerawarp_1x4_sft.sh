8bc9adbd2d755d8001e45664545d9f4932b91b19
maxtir <maxtirerror@gmail.com>
2025-04-22 04:29:40 +0000

    try sft setting

--- train_camerawarp_1x4_sft.sh
+++ train_camerawarp_1x4_sft.sh
@@ -34,10 +34,9 @@ DATA_ARGS=(
 TRAIN_ARGS=(
     --train_epochs 500 # number of training epochs
     --seed 42 # random seed
-    --batch_size 2
+    --batch_size 1
     --gradient_accumulation_steps 1
     --mixed_precision "bf16"  # ["no", "fp16"] # Only CogVideoX-2B supports fp16 training
-    --gradient_checkpointing
 )
 
 # System Configuration
