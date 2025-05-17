d3b6fcac80f76f3da832a68ffdea7b1f6bcdd9e8
maxtir <maxtirerror@gmail.com>
2025-05-17 03:51:11 +0000

    one bs mode

--- framepack_debug_lora.sh
+++ framepack_debug_lora.sh
@@ -32,7 +32,7 @@ DATA_ARGS=(
 TRAIN_ARGS=(
     --train_epochs 10 # number of training epochs
     --seed 42 # random seed
-    --batch_size 1
+    --batch_size 2
     --gradient_accumulation_steps 1
     --mixed_precision "bf16"  # ["no", "fp16"] # Only CogVideoX-2B supports fp16 training
     --gradient_checkpointing
